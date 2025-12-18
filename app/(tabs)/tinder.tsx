13c5f191fe78de57b0fdb91e771113fff7842615
deni-stwn <denistwn67@gmail.com>
2025-12-18 14:18:39 +0700

    simple tinder app

---
+++ app/(tabs)/tinder.tsx
@@ -0,0 +1,13 @@
+import TinderStack from '@/components/TinderStack'
+import React from 'react'
+import { View } from 'react-native'
+
+const Tinder = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TinderStack />
+    </View>
+  )
+}
+
+export default Tinder
